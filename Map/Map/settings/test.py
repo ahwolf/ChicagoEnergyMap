c22a94041fcaebb52b8fcff80e6278952991ec50
Aaron <aaron@aaronwolfsmbp2.att.net>
2014-03-19 11:32:26 -0400

    added django project

---
+++ Map/Map/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = ')(@j!4b@+g3%p$ikjhz*56hn@b)j%jlg@58gl5xh-$dxc)1bq4'
